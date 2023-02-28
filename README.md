446ae5dbfbb6c79d9b268069e772e24f7f4a9346
Qasura75 <112414285+Qasura75@users.noreply.github.com>
2023-02-28 17:11:02 +0000

    Finished game function - Finished project and updated README

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # rock-paper-scissor
 In this project we are going to code a simple rock-paper-scissor game that you can play with the computer
 
+------------------------------------------------------
+
 Pseudocode:
 Get the input from the user (can be rock, paper or scissor)
 Save Input in a variable 
@@ -12,3 +14,15 @@ If the scenario(User: Scissor - PC:Paper) = Won
 If the scenario(User: Paper - PC:Stone) = Won
 If both get the same item = Tie
 Otherwise = Lost
+
+In the function game() we play 5 round with a loop
+After 5 rounds the function sees who has more score and declares the winner
+
+------------------------------------------------------
+
+What I have learnt:
+- Loops
+- How to correctly use return
+- When to use const variables
+- How to write Pseudocode
+- How to comment on the project
--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-/*
-Take the user input and the computer input
-If the scenario(User: Stone - PC:Scissor) = Won
-If the scenario(User: Scissor - PC:Paper) = Won
-If the scenario(User: Paper - PC:Stone) = Won
-If both get the same item = Tie
-Otherwise = Lost*/
-const playerSelection = getPlayerSelection();
-const computerSelection = getComputerSelection();
+const playerSelection = 0;
+const computerSelection = 0;
 
-playRound(playerSelection, computerSelection);
+let playerScore = 0;
+let computerScore = 0;
 
 
+game();
 
 
 //Get the input from the user
@@ -29,26 +24,42 @@ function getComputerSelection() {
 
 
 
-
+//Take the user input and the computer input and evaluate who the winner is
 function playRound(playerSelection, computerSelection) {
 
     console.log("USER:" + playerSelection + "   -   " + "PC:" + computerSelection);
 
     if (playerSelection == "rock" && computerSelection == "scissor" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissor" && computerSelection == "paper") {
-        console.log("You have won!");
+        playerScore++;
+        console.log("You have won! - Score: USER:" + playerScore + " - " + "PC:" + computerScore);
     }
 
     else if (playerSelection == "scissor" && computerSelection == "rock" || playerSelection == "rock" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "scissor") {
-        console.log("You have lost!");
+        computerScore++;
+        console.log("You have lost! - Score: USER:" + playerScore + " - " + "PC:" + computerScore);
     }
 
     else {
-        console.log("It is a tie!");
+        console.log("It is a tie! - Score: USER:" + playerScore + " - " +   "PC:" + computerScore);
     }
 }
 
 
-/*
+//Repeats 5 rounds and then decides who has won
 function game() {
-    
-}*/
+    for (let i = 0; i < 5; i++) {
+        playRound(getPlayerSelection(), getComputerSelection());
+     }
+
+     if (playerScore > computerScore) {
+        console.log("Congratulations! You have won the game! - Score: USER:" + playerScore + " - " + "PC:" + computerScore);
+     }
+
+     else if (playerScore < computerScore) {
+        console.log("Unfortunately, you have lost the game! - Score: USER:" + playerScore + " - " + "PC:" + computerScore);
+     }
+
+     else {
+        console.log("Well played, but it is a tie! - Score: USER:" + playerScore + " - " +   "PC:" + computerScore);
+    }
+}
